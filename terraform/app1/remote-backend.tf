3b213a392e2cb67013516e85e4cfb766aace4e9c
Gangadhar Rayudu <grayudu@gmail.com>
2019-04-30 19:29:13 -0700

    updated with key tag

--- terraform/app1/remote-backend.tf
+++ terraform/app1/remote-backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket         = "terraform-statelock-store-useast" # change it to the name of the name of your bucket
-    key            = "dev"
+    key            = "_key_"
     region         = "us-east-1"
     profile        = "grayudu"
     dynamodb_table = "terraform-statelock-useast"
