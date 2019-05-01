d2b82b3050d69777699f0d42ed10a73264fa37bd
Gangadhar Rayudu <grayudu@gmail.com>
2019-05-01 06:10:00 -0700

    updated smoketest

--- terraform/app1/smoketest.sh
+++ terraform/app1/smoketest.sh
@@ -1,11 +1,18 @@
+n=1
 while true
 do 
 	i=`cat /tmp/endpoint`
 	status=`curl -s -o /dev/null -w  "%{http_code}" http://$i`
 	if [ $status -eq 200 ]; then 
-		echo "Smoke Successfull"
+		echo "SmokeTest Successfull"
 		break;
 	else 
 		echo "......"
+                sleep 10
+                n=$(( n+1 ))
+                if [ $n -gt 4 ]; then
+                    echo "SmokeTest failed"
+                    break;
+                fi
 	fi
 done
