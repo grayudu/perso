6d53b2a13811a8e136ade456240ae6c7fa6567d7
Gangadhar Rayudu <grayudu@gmail.com>
2019-06-05 17:36:21 -0400

    updated

--- lab/terraform/demo/remote-backend.tf
+++ lab/terraform/demo/remote-backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "terraform-state-ganga-us-east-2" # change it to the name of the name of your bucket
+    bucket         = "terraform-state-ganga-_region_" # change it to the name of the name of your bucket
     key            = "dev-_region_"
     region         = "_region_"
     profile        = "grayudu"
--- lab/terraform/otc/remote-backend.tf
+++ lab/terraform/otc/remote-backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "terraform-state-ganga-us-east-2" # change it to the name of the name of your bucket
+    bucket         = "terraform-state-ganga-_region_" # change it to the name of the name of your bucket
     key            = "otc-_region_"
     region         = "_region_"
     profile        = "grayudu"
